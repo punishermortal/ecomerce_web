8629b35131086dda8cb10ea8f04207904c0af72f
Rupashree Roy <rupashreesmagic@gmail.com>
2025-11-11 21:30:55 +0530

    aa

--- backend/nextbloom/settings.py
+++ backend/nextbloom/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-change-in-production'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
+ALLOWED_HOSTS =['*']# config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
 
 # Application definition
 INSTALLED_APPS = [
@@ -147,12 +147,13 @@ SIMPLE_JWT = {
 }
 
 # CORS Settings
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "http://21.2.23.24",
-    "https://21.2.23.24",
-]
+CORS_ALLOW_ALL_ORIGINS = True
+# [
+#     "http://localhost:3000",
+#     "http://127.0.0.1:3000",
+#     "http://21.2.23.24",
+#     "https://21.2.23.24",
+# ]
 
 CORS_ALLOW_CREDENTIALS = True
 
